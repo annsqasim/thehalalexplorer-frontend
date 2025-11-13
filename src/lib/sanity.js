90ce444c6f2c381e19ff0d9667c008f9ef91dbd7
Anns Qasim <anns.qasim@gmail.com>
2025-11-14 00:33:40 +0400

    changing api version to stable date

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: '46z29k3b', // 👈 replace this
   dataset: 'production',
-  apiVersion: '2025-11-14', // use a fixed date
+  apiVersion: "2023-08-01", // use a fixed date
   useCdn: false, // `false` if you want fresh data
 })
